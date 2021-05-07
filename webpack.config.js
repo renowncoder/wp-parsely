dd10aae56d1c00c9965a815fcb78dde3b5f90d78
Jeff Bowen <jblz@users.noreply.github.com>
2021-05-06 21:16:16 -0400

    pull entrypoints into a webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require( 'path' );
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+module.exports = {
+	...defaultConfig,
+	resolve: {
+		modules: [ path.resolve( __dirname, 'src', 'js' ), 'node_modules' ],
+	},
+	entry: {
+		'init-api': path.resolve( __dirname, 'src', 'js', 'lib', 'init-api' ),
+		'admin-page': path.resolve( __dirname, 'src', 'js', 'lib', 'admin-page' ),
+		'recommended-widget': path.resolve( __dirname, 'src', 'js', 'recommended-widget' ),
+	},
+};
